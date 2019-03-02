39f2895d3f8606cde25dc92d5e93e3f692e55976
onireto <onireto@mcmaster.ca>
2019-03-02 06:07:40 +0000

    Part2

---
+++ Project01/README.md
@@ -0,0 +1,27 @@
+#CS1XA3- Project Analyzer
+
+Course: CS1XA3. Instructor: Curtis D'Alves. Session: Winter 2019
+
+##Setup
+
+Add project_analyzer.sh file to root repository to use.
+
+##Usage
+
+To use the features type: bash project_analyser.sh *method*.
+
+##Features
+
+###easygit
+The easygit function allows automatic github exchange and put items to the repository. Selecting 'D' will do a full git add, while selecting 'F' adds a specified file.
+
+###TODOlog
+The TODOlog function creates a list items of all TODO tags
+
+###logError
+The logError functions logs all error in all the Haskell and Python files
+
+###fileCount
+The fileCount functions returns the number of HTML, Javascript, CSS, Python, Haskell, CSS and Bash Script files in the current directory
+
+
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -7,11 +7,32 @@ echo "TODO is placed in todo.log";
 
 function fileCount(){
 # Outputs the count of haskell, java script, css , python, html and bash script files
-haskell=find . -type f - name ".hs" | wc -l;
-javascript=find . -type f - name ".js" | wc -l;
-css=find . -type f - name ".css" | wc -l;
-python=find  . - type f - name ".py" | wc -l;
-html_v=find . - type f - name ".html" | wc -l;
-bash_v=find . - type f - name ".sh" |wc -l;
+haskell=$(find . -type f - name ".hs" | wc -l);
+javascript=$(find . -type f - name ".js" | wc -l);
+css=$(find . -type f - name ".css" | wc -l);
+python=$(find  . - type f - name ".py" | wc -l);
+html_v=$(find . - type f - name ".html" | wc -l);
+bash_v=$(find . - type f - name ".sh" |wc -l);
 echo "Haskell: $haskell, Javascript: $javascript, CSS: $css, Python: $python, HTML: $html_v, and Bash Script: $bash_v"; 
+}
+function easygit(){
+ echo "Whole  directory or one fil? [D/F]:"
+read resp
+if [$resp == "D"]
+then 
+	git add * ;
+else
+	echo "Enter file name"
+	read file
+	git add $file
+fi
+	echo "Enter commit message";
+	read msg
+	git commit -m $msg;
+	git push origin master;
+}
+function logError(){
+ find . -name "*hs" -exec ghc  -fno -code {} \; 2>compile_fail.log
+find . -name "*py" -exec python -fno -code {} \; 2>>compile_fail.log
+echo"Compile Error log created";
 }
